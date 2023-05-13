3db59b8110ad5e8025b8fbd26f578696894b3488
Paolo Rechia <paolorechia@gmail.com>
2023-05-14 00:46:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+requirements = read("requirements.txt")
+
+setup(
+    name="code_it",
+    version="0.1.0",
+    author="Paolo Rechia",
+    author_email="paolorechia@gmail.com",
+    maintainer="Paolo Rechia",
+    maintainer_email="paolorechia@gmail.com",
+    license="MIT",
+    url="https://github.com/paolorechia/code-it",
+    description="Tooling to generate and execute code",
+    python_requires=">=3.9",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
